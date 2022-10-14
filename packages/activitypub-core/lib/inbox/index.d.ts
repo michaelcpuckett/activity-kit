6eded7f8e769d6b713d93da3b905f523656c602a
Michael Puckett <michaelcpuckett@gmail.com>
2022-10-14 08:52:28 -0400

    pkg issues

--- packages/activitypub-core/lib/inbox/index.d.ts
+++ packages/activitypub-core/lib/inbox/index.d.ts
@@ -5,7 +5,7 @@ import type { Database } from 'activitypub-core-types';
 import { DeliveryService } from 'activitypub-core-delivery';
 export declare function inboxHandler(req: IncomingMessage, res: ServerResponse, databaseService: Database, deliveryService: DeliveryService): Promise<{
     props?: {
-        entity?: AP.OrderedCollection | import("activitypub-core-types/lib/src/Core/Link").BaseLink | import("activitypub-core-types/lib/src/Core").Mention | AP.Article | AP.Event | AP.Note | AP.Page | AP.Place | AP.Relationship | AP.Tombstone | AP.Profile | AP.Document | AP.Application | AP.Service | AP.Group | AP.Organization | AP.Person | AP.Accept | AP.Follow | AP.Delete | AP.Create | AP.Arrive | AP.Add | AP.Offer | AP.Like | AP.Leave | AP.Ignore | AP.Join | AP.Reject | AP.View | AP.Update | AP.Undo | AP.Remove | AP.Read | AP.Listen | AP.Move | AP.Travel | AP.Announce | AP.Flag | AP.Dislike | AP.Question | AP.Collection | AP.CollectionPage | AP.OrderedCollectionPage;
+        entity?: AP.OrderedCollection | import("activitypub-core-types/lib/activitypub/Core/Link").BaseLink | import("activitypub-core-types/lib/activitypub/Core").Mention | AP.Article | AP.Event | AP.Note | AP.Page | AP.Place | AP.Relationship | AP.Tombstone | AP.Profile | AP.Document | AP.Application | AP.Service | AP.Group | AP.Organization | AP.Person | AP.Accept | AP.Follow | AP.Delete | AP.Create | AP.Arrive | AP.Add | AP.Offer | AP.Like | AP.Leave | AP.Ignore | AP.Join | AP.Reject | AP.View | AP.Update | AP.Undo | AP.Remove | AP.Read | AP.Listen | AP.Move | AP.Travel | AP.Announce | AP.Flag | AP.Dislike | AP.Question | AP.Collection | AP.CollectionPage | AP.OrderedCollectionPage;
     };
 } | {
     props: {};
