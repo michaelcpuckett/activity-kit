7ec3086a5f681ac1f2d3266342e94e6ed7f23cb6
Michael Puckett <michaelcpuckett@gmail.com>
2022-10-19 07:31:36 -0400

    Tests

--- packages/activitypub-core-delivery/src/getPrivateKey.ts
+++ packages/activitypub-core-delivery/src/getPrivateKey.ts
@@ -3,20 +3,21 @@ import { AP } from 'activitypub-core-types';
 
 export async function getPrivateKey(this: DeliveryService, actor: AP.Actor) {
   if (!actor.preferredUsername) {
-    throw new Error('Bad actor');
+    throw new Error('Actor has no `preferredUsername`.');
   }
 
   const userId = await this.databaseService.findStringIdByValue(
     'username',
     actor.preferredUsername,
   );
+
   const privateKey = await this.databaseService.findStringValueById(
     'private-key',
     userId,
   );
 
   if (!privateKey) {
-    throw new Error("User's private key not found.");
+    throw new Error('Private key not found for this Actor.');
   }
 
   return privateKey;
--- packages/activitypub-core-firebase-authentication/__tests__/activitypub-core-firebase-auth.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const activitypubCoreFirebaseAuth = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(activitypubCoreFirebaseAuth(), 'Hello from activitypubCoreFirebaseAuth');
-console.info("activitypubCoreFirebaseAuth tests passed");
