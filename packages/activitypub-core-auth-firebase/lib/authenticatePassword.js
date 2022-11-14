244f5c5758f461395b2df29bd38bdee7682d0ee5
Michael Puckett <michaelcpuckett@gmail.com>
2022-11-14 10:04:08 -0500

    authenicate

--- packages/activitypub-core-auth-firebase/lib/authenticatePassword.js
+++ packages/activitypub-core-auth-firebase/lib/authenticatePassword.js
@@ -4,6 +4,7 @@ exports.authenticatePassword = void 0;
 const auth_1 = require("firebase/auth");
 const app_1 = require("firebase/app");
 async function authenticatePassword(email, password) {
+    console.log('AUTHENTICATE PASSWORD');
     const app = (0, app_1.initializeApp)(this.appOptions);
     const auth = (0, auth_1.getAuth)(app);
     const user = await (0, auth_1.signInWithEmailAndPassword)(auth, email, password);
--- packages/activitypub-core-auth-firebase/src/authenticatePassword.ts
+++ packages/activitypub-core-auth-firebase/src/authenticatePassword.ts
@@ -7,6 +7,7 @@ export async function authenticatePassword(
   email: string,
   password: string
 ): Promise<Boolean> {
+  console.log('AUTHENTICATE PASSWORD');
   const app = initializeApp(this.appOptions);
   const auth = getAuth(app);
   const user = await signInWithEmailAndPassword(auth, email, password);
