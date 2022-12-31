acbf63cb019c2e37b79655ae0837e7ee59f13b8e
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-12-31 02:44:50 -0600

    rename items -> entityItems to avoid duplicate var error

--- packages/activitypub-core-endpoints/src/entity/index.ts
+++ packages/activitypub-core-endpoints/src/entity/index.ts
@@ -142,8 +142,8 @@ export class EntityGetEndpoint {
     const typeFilter = query.has('type') ? query.get('type').split(',') : [];
     const sort = query.get('sort');
     const limit = query.has('limit') ? Number(query.get('limit')) : ITEMS_PER_COLLECTION_PAGE;
-    const items = entity[isOrderedCollection ? 'orderedItems' : 'items']
-    const expandedItems = await Promise.all((Array.isArray(items) ? items : []).map(async (id: URL) => {
+    const entityItems = entity[isOrderedCollection ? 'orderedItems' : 'items']
+    const expandedItems = await Promise.all((Array.isArray(entityItems) ? entityItems : []).map(async (id: URL) => {
       return await this.adapters.db.queryById(id);
     }));
     const filteredItems = typeFilter.length ? expandedItems.filter(({ type }) => typeFilter.includes(type)) : expandedItems;
