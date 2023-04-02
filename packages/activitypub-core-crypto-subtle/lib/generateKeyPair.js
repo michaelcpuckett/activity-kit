dd59aabcb4178aaf63f94523fbb2410804d0b65a
Michael Puckett <michaelcpuckett@gmail.com>
2023-04-02 12:14:36 -0400

    fix subtle

--- packages/activitypub-core-crypto-subtle/lib/generateKeyPair.js
+++ packages/activitypub-core-crypto-subtle/lib/generateKeyPair.js
@@ -11,6 +11,30 @@ async function generateKeyPair() {
         publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
         hash: 'SHA-256',
     }, true, ['encrypt', 'decrypt']);
+    const privateKeyToPEM = async (key) => {
+        const rawPrivateKey = await crypto.subtle.exportKey('pkcs8', key);
+        const arrayPrivateKey = new Uint8Array(rawPrivateKey);
+        const pemHeader = '-----BEGIN PRIVATE KEY-----\n';
+        const pemFooter = '-----END PRIVATE KEY-----\n';
+        const base64PrivateKey = btoa(String.fromCharCode(...arrayPrivateKey));
+        let pemBody = '';
+        for (let i = 0; i < base64PrivateKey.length; i += 64) {
+            pemBody += base64PrivateKey.slice(i, i + 64) + '\n';
+        }
+        return pemHeader + pemBody + pemFooter;
+    };
+    const publicKeyToPEM = async (key) => {
+        const rawPublicKey = await crypto.subtle.exportKey('spki', key);
+        const arrayPublicKey = new Uint8Array(rawPublicKey);
+        const pemHeader = '-----BEGIN PUBLIC KEY-----\n';
+        const pemFooter = '-----END PUBLIC KEY-----\n';
+        const base64PublicKey = btoa(String.fromCharCode(...arrayPublicKey));
+        let pemBody = '';
+        for (let i = 0; i < base64PublicKey.length; i += 64) {
+            pemBody += base64PublicKey.slice(i, i + 64) + '\n';
+        }
+        return pemHeader + pemBody + pemFooter;
+    };
     const privateKeyPEM = await privateKeyToPEM(keys.privateKey);
     const publicKeyPEM = await publicKeyToPEM(keys.publicKey);
     return {
@@ -19,28 +43,4 @@ async function generateKeyPair() {
     };
 }
 exports.generateKeyPair = generateKeyPair;
-async function privateKeyToPEM(key) {
-    const rawPrivateKey = await crypto.subtle.exportKey('pkcs8', key);
-    const arrayPrivateKey = new Uint8Array(rawPrivateKey);
-    const pemHeader = '-----BEGIN PRIVATE KEY-----\n';
-    const pemFooter = '-----END PRIVATE KEY-----\n';
-    const base64PrivateKey = btoa(String.fromCharCode(...arrayPrivateKey));
-    let pemBody = '';
-    for (let i = 0; i < base64PrivateKey.length; i += 64) {
-        pemBody += base64PrivateKey.slice(i, i + 64) + '\n';
-    }
-    return pemHeader + pemBody + pemFooter;
-}
-async function publicKeyToPEM(key) {
-    const rawPublicKey = await crypto.subtle.exportKey('spki', key);
-    const arrayPublicKey = new Uint8Array(rawPublicKey);
-    const pemHeader = '-----BEGIN PUBLIC KEY-----\n';
-    const pemFooter = '-----END PUBLIC KEY-----\n';
-    const base64PublicKey = btoa(String.fromCharCode(...arrayPublicKey));
-    let pemBody = '';
-    for (let i = 0; i < base64PublicKey.length; i += 64) {
-        pemBody += base64PublicKey.slice(i, i + 64) + '\n';
-    }
-    return pemHeader + pemBody + pemFooter;
-}
 //# sourceMappingURL=generateKeyPair.js.map
--- packages/activitypub-core-crypto-subtle/src/generateKeyPair.ts
+++ packages/activitypub-core-crypto-subtle/src/generateKeyPair.ts
@@ -19,6 +19,38 @@ export async function generateKeyPair(this: SubtleCryptoAdapter): Promise<{
     ['encrypt', 'decrypt'],
   );
 
+  const privateKeyToPEM = async (key: CryptoKey): Promise<string> => {
+    const rawPrivateKey = await crypto.subtle.exportKey('pkcs8', key);
+    const arrayPrivateKey = new Uint8Array(rawPrivateKey);
+
+    const pemHeader = '-----BEGIN PRIVATE KEY-----\n';
+    const pemFooter = '-----END PRIVATE KEY-----\n';
+    const base64PrivateKey = btoa(String.fromCharCode(...arrayPrivateKey));
+
+    let pemBody = '';
+    for (let i = 0; i < base64PrivateKey.length; i += 64) {
+      pemBody += base64PrivateKey.slice(i, i + 64) + '\n';
+    }
+
+    return pemHeader + pemBody + pemFooter;
+  };
+
+  const publicKeyToPEM = async (key: CryptoKey): Promise<string> => {
+    const rawPublicKey = await crypto.subtle.exportKey('spki', key);
+    const arrayPublicKey = new Uint8Array(rawPublicKey);
+
+    const pemHeader = '-----BEGIN PUBLIC KEY-----\n';
+    const pemFooter = '-----END PUBLIC KEY-----\n';
+    const base64PublicKey = btoa(String.fromCharCode(...arrayPublicKey));
+
+    let pemBody = '';
+    for (let i = 0; i < base64PublicKey.length; i += 64) {
+      pemBody += base64PublicKey.slice(i, i + 64) + '\n';
+    }
+
+    return pemHeader + pemBody + pemFooter;
+  };
+
   const privateKeyPEM = await privateKeyToPEM(keys.privateKey);
   const publicKeyPEM = await publicKeyToPEM(keys.publicKey);
 
@@ -26,36 +58,4 @@ export async function generateKeyPair(this: SubtleCryptoAdapter): Promise<{
     privateKey: privateKeyPEM,
     publicKey: publicKeyPEM,
   };
-}
-
-async function privateKeyToPEM(key: CryptoKey): Promise<string> {
-  const rawPrivateKey = await crypto.subtle.exportKey('pkcs8', key);
-  const arrayPrivateKey = new Uint8Array(rawPrivateKey);
-
-  const pemHeader = '-----BEGIN PRIVATE KEY-----\n';
-  const pemFooter = '-----END PRIVATE KEY-----\n';
-  const base64PrivateKey = btoa(String.fromCharCode(...arrayPrivateKey));
-
-  let pemBody = '';
-  for (let i = 0; i < base64PrivateKey.length; i += 64) {
-    pemBody += base64PrivateKey.slice(i, i + 64) + '\n';
-  }
-
-  return pemHeader + pemBody + pemFooter;
-}
-
-async function publicKeyToPEM(key: CryptoKey): Promise<string> {
-  const rawPublicKey = await crypto.subtle.exportKey('spki', key);
-  const arrayPublicKey = new Uint8Array(rawPublicKey);
-
-  const pemHeader = '-----BEGIN PUBLIC KEY-----\n';
-  const pemFooter = '-----END PUBLIC KEY-----\n';
-  const base64PublicKey = btoa(String.fromCharCode(...arrayPublicKey));
-
-  let pemBody = '';
-  for (let i = 0; i < base64PublicKey.length; i += 64) {
-    pemBody += base64PublicKey.slice(i, i + 64) + '\n';
-  }
-
-  return pemHeader + pemBody + pemFooter;
 }
