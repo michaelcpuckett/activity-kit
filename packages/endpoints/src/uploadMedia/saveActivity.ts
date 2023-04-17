45d7a7778e643f06c8d72ac2249853c870539073
Michael Puckett <michaelcpuckett@gmail.com>
2023-04-17 05:12:51 -0400

    Remove likes etc from activity

--- packages/endpoints/src/uploadMedia/saveActivity.ts
+++ packages/endpoints/src/uploadMedia/saveActivity.ts
@@ -16,7 +16,7 @@ export async function saveActivity(this: UploadMediaPostEndpoint) {
 
   const publishedDate = new Date();
 
-  // TODO format replies...
+  // TODO format URLs...
 
   const objectReplies: AP.Collection = {
     '@context': new URL(ACTIVITYSTREAMS_CONTEXT),
@@ -65,51 +65,6 @@ export async function saveActivity(this: UploadMediaPostEndpoint) {
 
   this.activity.object.published = publishedDate;
 
-  const activityReplies: AP.Collection = {
-    '@context': new URL(ACTIVITYSTREAMS_CONTEXT),
-    id: new URL(`${this.activity.id.toString()}/replies`),
-    url: new URL(`${this.activity.id.toString()}/replies`),
-    name: 'Replies',
-    type: AP.CollectionTypes.COLLECTION,
-    totalItems: 0,
-    items: [],
-    published: publishedDate,
-  };
-
-  const activityLikes: AP.OrderedCollection = {
-    '@context': new URL(ACTIVITYSTREAMS_CONTEXT),
-    id: new URL(`${this.activity.id.toString()}/likes`),
-    url: new URL(`${this.activity.id.toString()}/likes`),
-    name: 'Likes',
-    type: AP.CollectionTypes.ORDERED_COLLECTION,
-    totalItems: 0,
-    orderedItems: [],
-    published: publishedDate,
-  };
-
-  const activityShares: AP.OrderedCollection = {
-    '@context': new URL(ACTIVITYSTREAMS_CONTEXT),
-    id: new URL(`${this.activity.id.toString()}/shares`),
-    url: new URL(`${this.activity.id.toString()}/shares`),
-    name: 'Shares',
-    type: AP.CollectionTypes.ORDERED_COLLECTION,
-    totalItems: 0,
-    orderedItems: [],
-    published: publishedDate,
-  };
-
-  if (activityLikes.id instanceof URL) {
-    this.activity.likes = activityLikes.id;
-  }
-
-  if (activityShares.id instanceof URL) {
-    this.activity.shares = activityShares.id;
-  }
-
-  if (activityReplies.id instanceof URL) {
-    this.activity.replies = activityReplies.id;
-  }
-
   this.activity.published = publishedDate;
 
   await Promise.all([
@@ -117,9 +72,6 @@ export async function saveActivity(this: UploadMediaPostEndpoint) {
     this.core.saveEntity(objectLikes),
     this.core.saveEntity(objectShares),
     this.core.saveEntity(this.activity.object as AP.Entity),
-    this.core.saveEntity(activityReplies),
-    this.core.saveEntity(activityLikes),
-    this.core.saveEntity(activityShares),
     this.core.saveEntity(this.activity as AP.Entity),
     this.core.insertOrderedItem(getId(this.actor?.outbox), this.activity.id),
   ]);
