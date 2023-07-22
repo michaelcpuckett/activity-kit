13dc5d88fce24e0ddad9896bd2047196d502fe78
Michael Puckett <michaelcpuckett@gmail.com>
2023-07-22 12:59:32 -0400

    fixup

--- packages/endpoints/lib/inbox/getActors.d.ts
+++ packages/endpoints/lib/inbox/getActors.d.ts
@@ -1,2 +1,2 @@
 import { InboxPostEndpoint } from '.';
-export declare function getActors(this: InboxPostEndpoint): Promise<any[]>;
+export declare function getActors(this: InboxPostEndpoint): Promise<(import("@activity-kit/types").Application | import("@activity-kit/types").Service | import("@activity-kit/types").Group | import("@activity-kit/types").Organization | import("@activity-kit/types").Person)[]>;
